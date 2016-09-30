7a51b6d1f42a5489303701068a1fe9f3222b949c
ksed <hydrogeo.coder@gmail.com>
2016-09-30 19:18:01 -0400

    added pause and reset buttons and logic, and got the current timer to display in the tab/document title

--- public/js/app.js
+++ public/js/app.js
@@ -3,30 +3,53 @@
   //initialize variables
   var startButton = $('#start');
   var breakButton = $('#break');
+  var pauseButton = $('#pause');
+  var resetButton = $('#reset');
+  var options = $('#options');
   var seconds = $('#seconds');
   var minutes = $('#minutes');
+  var docTitle = $(document).find('title');
   var timerInterval;
   var isOnBreak = false;
+  var timerMinutes = '25';
+  var timerSeconds = '00';
+  var breakMinutes = '05';
+  var breakSeconds = '00';
 
   //main functionality
   startButton.on('click', startTimer);
   breakButton.on('click', startBreak);
+  pauseButton.on('click', pauseTimer);
+  resetButton.on('click', resetTimer);
 
   //function definitions
   function startBreak() {
     // set isOnBreak true
     isOnBreak = true;
-    // set the minutes to 5 and seconds to 0
-    minutes.text('05');
-    seconds.text('00');
+    // set the break minutes and seconds
+    minutes.text(breakMinutes);
+    seconds.text(breakSeconds);
     // hide the break button and start the timer
     breakButton.hide();
     startTimer();
   }
   function startTimer() {
     if (!timerInterval) {
+      setDocTitle();
       timerInterval = setInterval(countdown, 1000);
     }
+  }
+  function pauseTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    startButton.attr('disabled', false);
+  }
+  function resetTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    minutes.text(timerMinutes);
+    seconds.text(timerSeconds);
+    startTimer();
   }
   function countdown(){
     var secondsText = seconds.text();
@@ -42,14 +65,17 @@
         startButton.attr('disabled', true);
         //unhide the break button
         breakButton.show();
+        options.hide();
       } else {
         isOnBreak = false;
         startButton.attr('disabled', false);
-        minutes.text('25');
-        seconds.text('00');
+        minutes.text(timerMinutes);
+        seconds.text(timerSeconds);
+        options.show();
       }
       return;
     }
+    if (minutes.text() == minutesTextAsNumber) { startButton.attr('disabled', true); }
     if (secondsTextAsNumber === 0) {
       // then change seconds text to 59 otherwise keep going
       if (minutesTextAsNumber !== 0) {
@@ -64,6 +90,7 @@
       var padSecondsTextAsNumber = pad(decreaseSecondsAsNumberByOne);
       seconds.text(padSecondsTextAsNumber);
     }
+    setDocTitle();
     // var secondsValue = parseInt(seconds.text());
     // seconds.text( pad( secondsValue - 1 ) );
   }
@@ -75,6 +102,14 @@
     } else {
       // spit out the original number
       return num;
+    }
+  }
+
+  function setDocTitle() {
+    if (isOnBreak) {
+      docTitle.text( 'OnBreak: '+minutes.text()+':'+seconds.text() );
+    } else {
+      docTitle.text( 'Working: '+minutes.text()+':'+seconds.text() );
     }
   }
 }());
